89740d47d3367ad7c7af823b4fb8ba889b226f74
Clara Freitas de Oliveira <clara.folive@gmail.com>
2026-06-19 15:38:50 -0300

    Update App.js
    adicionei as outras telas criadas

--- codigo-fonte/src/App.js
+++ codigo-fonte/src/App.js
@@ -2,9 +2,13 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Tela_BemVindo from './components/Tela_BemVindo';
-import Tela_Login from './components/Tela_Login';
-import Tela_Registro from './components/Tela_Registro';
+import Tela_BemVindo from './src/components/Tela_BemVindo';
+import Tela_Login from './src/components/Tela_Login';
+import Tela_Registro from './src/components/Tela_Registro';
+import Tela_Inicio from './src/components/Tela_Inicio';
+import Tela_Perfil from './src/components/Tela_Perfil';
+import Tela_Geladeira from './src/components/Tela_Geladeira';
+import Tela_Config from './src/components/Tela_Config';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +22,12 @@ export default function App() {
         <Stack.Screen name="Tela_BemVindo" component={Tela_BemVindo} />
         <Stack.Screen name="Tela_Login" component={Tela_Login} />
         <Stack.Screen name="Tela_Registro" component={Tela_Registro} />
+        <Stack.Screen name="Tela_Inicio" component={Tela_Inicio} />
+        <Stack.Screen name="Tela_Perfil" component={Tela_Perfil} />
+        <Stack.Screen name="Tela_Geladeira" component={Tela_Geladeira} />
+        <Stack.Screen name="Tela_Config" component={Tela_Config} />
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
